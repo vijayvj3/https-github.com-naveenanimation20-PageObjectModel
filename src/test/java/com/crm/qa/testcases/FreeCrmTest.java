b6e285f145e70e82767ee2f216250ba4ce879440
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2019-11-29 16:34:27 +0530

    Update FreeCrmTest.java

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -7,7 +7,7 @@ public class TestChrome {
  
 public static void main(String[] args) {
  
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\vi20090299\/Downloads\\New folder\\chromedriver_win32\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\vi20090299\\Downloads\\New folder\\chromedriver_win32\\chromedriver.exe");
  
 // Initialize browser
 WebDriver driver=new ChromeDriver();
